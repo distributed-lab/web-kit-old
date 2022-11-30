a7a313be6eb2439624226772560e3ea3d21962ac
Yehor <yehor.podporinov@gmail.com>
2022-11-30 17:35:06 +0200

    added generic types to methods

--- packages/json-api-client/src/utils/json-api-body-builder.ts
+++ packages/json-api-client/src/utils/json-api-body-builder.ts
@@ -6,52 +6,54 @@ import {
 } from '@/types'
 
 export class JsonApiBodyBuilder {
-  private body: JsonApiRecord
+  #body: JsonApiRecord
 
   constructor() {
-    this.body = {
+    this.#body = {
       data: {
         type: '',
       },
     }
   }
 
-  public setData(data: JsonApiRecordData): JsonApiBodyBuilder {
-    this.body.data = data
+  public setData<T extends string = string>(
+    data: JsonApiRecordData<T>,
+  ): JsonApiBodyBuilder {
+    this.#body.data = data
     return this
   }
 
   public setIncluded(
     included: InstanceType<typeof JsonApiBodyBuilder>[],
   ): JsonApiBodyBuilder {
-    this.body.included = included.map(record => record.build())
+    this.#body.included = included.map(record => record.build())
     return this
   }
 
-  public setType(type: string): JsonApiBodyBuilder {
-    this.body.data.type = type
+  public setType<T extends string = string>(type: T): JsonApiBodyBuilder {
+    this.#body.data.type = type
     return this
   }
 
   public setID(id: string): JsonApiBodyBuilder {
-    this.body.data.id = id
+    this.#body.data.id = id
     return this
   }
 
   public setAttributes(attributes: JsonApiAttributes): JsonApiBodyBuilder {
-    this.body.data.attributes = attributes
+    this.#body.data.attributes = attributes
     return this
   }
 
-  public setRelationships(
-    relationships: JsonApiRelationships,
+  public setRelationships<T extends string = string>(
+    relationships: JsonApiRelationships<T>,
   ): JsonApiBodyBuilder {
-    this.body.data.relationships = relationships
+    this.#body.data.relationships = relationships
     return this
   }
 
   public build(): JsonApiRecord {
-    const result: JsonApiRecord = { ...this.body }
+    const result: JsonApiRecord = { ...this.#body }
     return result
   }
 }
