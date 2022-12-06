512cbda8110ff4bd70ecea9d593a1025ab7c83f6
Sorizen <anderteiker505@gmail.com>
2022-12-06 15:37:32 +0200

    fix

--- packages/utils/src/browser.ts
+++ packages/utils/src/browser.ts
@@ -7,4 +7,4 @@ import * as utils from './index'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ;(window as any).webKitUtils = utils
 
-console.warn('Web Kit JSON API Client was added to the window object.')
+console.warn('Web Kit Utils was added to the window object.')
