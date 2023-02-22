4c4136b21e510c56dd868b1f0d7922679e7e8be3
Semen Loktionov <loktionov.kh@gmail.com>
2023-02-22 23:00:37 +0200

    add check event, add once, fix change log and package version

--- packages/utils/src/event-emitter.ts
+++ packages/utils/src/event-emitter.ts
@@ -1,19 +1,39 @@
-import { EventKey, EventMap, EventHandler, EventHandlers } from '@/types'
+import { EventMapKey, EventMap, EventHandler, EventHandlersMap } from '@/types'
 
 export class EventEmitter<T extends EventMap> {
-  #handlers: EventHandlers<T> = {}
+  #handlers: EventHandlersMap<T> = {}
 
-  public on<K extends EventKey<T>>(key: K, fn: EventHandler<T[K]>): void {
+  get handlers(): EventHandlersMap<T> {
+    return this.#handlers
+  }
+
+  public on<K extends EventMapKey<T>>(key: K, fn: EventHandler<T[K]>): void {
     this.#handlers[key] = (this.#handlers[key] || [])?.concat(fn)
   }
 
-  public off<K extends EventKey<T>>(key: K, fn: EventHandler<T[K]>): void {
+  public once<K extends EventMapKey<T>>(key: K, fn: EventHandler<T[K]>): void {
+    const handler = (data: T[K]) => {
+      fn(data)
+      this.off(key, handler)
+    }
+    this.on(key, handler)
+  }
+
+  public off<K extends EventMapKey<T>>(key: K, fn: EventHandler<T[K]>): void {
+    this.#checkEvent(key)
     this.#handlers[key] = (this.#handlers[key] || [])?.filter(f => f !== fn)
   }
 
-  public emit<K extends EventKey<T>>(key: K, data: T[K]): void {
+  public emit<K extends EventMapKey<T>>(key: K, data: T[K]): void {
+    this.#checkEvent(key)
     ;(this.#handlers[key] || [])?.forEach((fn: EventHandler<T[K]>) => {
       fn(data)
     })
+  }
+
+  #checkEvent<K extends EventMapKey<T>>(key: K): void {
+    if (!this.#handlers[key]?.length) {
+      throw new TypeError(`Handlers for the event: ${key} not found`)
+    }
   }
 }
